f345de14f30604c5b460b9178e3f7d6976a9a49f
parhamfarsi <123801469+parhamfarsi@users.noreply.github.com>
2025-09-26 00:48:35 +0330

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ import read_csv as read
 import date 
 import clean_data as clean
 import analysis
+import functions as f
+import label
+import test2 as tst
+import indicators as indc
 
-file_path = 'Bitcoin Historical Data (1).csv'
+file_path = 'XRP Historical Data.csv'
 
 all_data = read.read_csv(file_path)
-data = date.date(all_data)
-data = clean.cleanData(data)
-print(analysis.analyze_indicators(data['High'], data['Low'], data['Price'], data['Vol.']))
+data = clean.cleanData(all_data)
+all_data = label.label2(all_data)
+all_data = indc.indicators(all_data)
+print(tst.test(all_data))
 
